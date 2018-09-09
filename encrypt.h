d87fb184bbe48ee33e1a7a0de129f72d952bcffb
r.j <articlemming@gmail.com>
2018-09-09 17:59:31 -0400

    almost working
    missing some digits

--- encrypt.h
+++ encrypt.h
@@ -2,10 +2,12 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<ctype.h>
 
 #define MAXINPUT 8192
 
 char *parsed_input;
+char output[MAXINPUT];
 size_t parse_input(char *out);
-char *encrypt(char *in);
+ void encrypt(char *in, char *out);
 size_t input_size;
